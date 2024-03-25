0805850c1ca943f90416977c6348483dd53853fe
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-03-25 19:48:10 +0200

    Removing dist directory

--- src/utils/loadConfig.js
+++ src/utils/loadConfig.js
@@ -1,9 +1,8 @@
-export async function loadConfig(file='/config.json') {
+export async function loadConfig(file='config.json') {
     try {
         const response = await fetch(file);
-        if (!response.ok) {
-            throw new Error('Failed to load configuration');
-        }
+        // console.log(await fetch(file));
+
         return await response.json();
     } catch (error) {
         console.log('Error loading config:', error);
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/Blog-Wall-vue-js/',
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/wlad1slav.github.io/Blog-Wall-vue-js" : "/",
 })
